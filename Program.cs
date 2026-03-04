f624a756b9d27b35da56c129d2f041d24594f51f
gerberth Antonio Englenton de León <tono27a89@gmail.com>
2026-03-04 15:39:34 -0600

    recopilacion

--- Program.cs
+++ Program.cs
@@ -15,7 +15,53 @@ namespace actividad_10__08
             int ID;
             string nombre;
             int opcion;
-           
+            do
+            {
+                Console.WriteLine("1. ingresar empleado");
+                Console.WriteLine("2. modificar");
+                Console.WriteLine("0. salir");
+                int.TryParse(Console.ReadLine(), out opcion);
+                switch (opcion)
+                {
+                    case 1:
+
+
+
+
+                        Console.WriteLine($"Ingrese Id de empleado");
+                        nombre = Console.ReadLine();
+                        if (empleado.ContainsKey(nombre))
+                        {
+                            Console.WriteLine("nombre ya ingresado ingrese otro");
+
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("ingrese notas:");
+                            double.TryParse(Console.ReadLine(), out notas);
+                            estudiantes.Add(nombre, notas);
+
+                        }
+
+                        break;
+                    case 2:
+                        Console.WriteLine("ingrese el nombre que desea ver notas");
+                        nombre = Console.ReadLine();
+                        Console.WriteLine("la nota de " + nombre + " es:" + estudiantes[nombre]);
+
+
+                        break;
+                    default:
+                        Console.WriteLine("ingrese valor valido");
+                        break;
+                }
+
+            }
+            while (opcion != 0);
+            Console.WriteLine("hasta luego");
+
+
         }
     }
 }
