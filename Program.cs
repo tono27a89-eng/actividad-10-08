4c974b420cc320eb37321c9d1b325a2738f8d6cf
gerberth Antonio Englenton de León <tono27a89@gmail.com>
2026-03-04 15:38:17 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace actividad_10__08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cree un diccionario de empleados. Modifique el nombre de uno usando la sintaxis diccionario[id] = nuevoNombre.");
+        Dictionary<int, string>empleado=new Dictionary<int, string>();
+            int ID;
+            string nombre;
+            int opcion;
+           
+        }
+    }
+}
